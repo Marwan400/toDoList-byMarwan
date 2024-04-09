67784e7ae552f8e4a9781008b0e40966cb094f69
Marwan400 <marwan40059@gmail.com>
2024-04-09 05:48:48 +0300

    Initial Commit

---
+++ toDoList-byMarwan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toDoList-byMarwan
+//
+//  Created by Marwan Al.Jabri on 30/09/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toDoList-byMarwan/toDoList_byMarwanApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoList_byMarwanApp.swift
+//  toDoList-byMarwan
+//
+//  Created by Marwan Al.Jabri on 30/09/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct toDoList_byMarwanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
